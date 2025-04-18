b3ecf59f321811cef35bef9a3b5a70fbd5881a9e
Alekonios <alekongames@gmail.com>
2025-04-18 17:55:28 +0300

    добавил пару анимаций

--- scenes/Entity/Player/PlayerComponents/Movement.gd
+++ scenes/Entity/Player/PlayerComponents/Movement.gd
@@ -2,19 +2,24 @@
 class_name Movement
 extends CharacterBody3D
 
+@export var _Components : Components
+
 @export_category("Stats")
-@export var StartSpeed : float = 5.0
-@export var StartJump : float = 2.0
+@export var StartSpeed : float = 2.0
+@export var StartJump : float = 3.0
 @export var StartGravity = 2
-var Speed
-var JumpVel
-var Gravity
+@export var RunSpeed : float = 2.0
+
+var Speed : float
+var JumpVel : float
+var Gravity : float
 
 @export_category("Nodes")
 @export var Model : Node3D
 @export var DebugMoveVis : Node3D
 @export var FloorJumpTimer : Timer
 
+var IsRunning = false
 var WantJump = true
 var is_walking: bool = false
 
@@ -22,25 +27,42 @@ func _ready() -> void:
 	Speed = StartGravity
 	JumpVel = StartJump
 	Gravity = StartGravity
+	RunSpeed = StartSpeed * RunSpeed
 
 func _physics_process(delta: float) -> void:
 	var InputDir = Input.get_vector("MoveUp", "MoveDown", "MoveLeft", "MoveRight")
 	var Direction = (transform.basis * Vector3(InputDir.x, 0.0, InputDir.y)).normalized()
+	_Components._StateMachine.State = _Components._StateMachine.States.BasicMovement
+	if Input.is_action_pressed("Run") and InputDir:
+		Speed = lerp(Speed, RunSpeed, 0.1)
+		IsRunning = true
+		_Components._StateMachine.Condition = _Components._StateMachine.BasicMovementStates.Run
+	else:
+		Speed = lerp(Speed, StartSpeed, 0.1)
+		IsRunning = false
+		
 	if Direction:
 		velocity.x = lerp(velocity.x, Direction.x * Speed, 0.2)
 		velocity.z = lerp(velocity.z, -Direction.z * Speed, 0.2)
 		DebugMoveVis.look_at(Direction + position)
 		Model.rotation.y = lerp_angle(Model.rotation.y, -DebugMoveVis.rotation.y, 0.2)
 		is_walking = true
+		if !IsRunning:
+			_Components._StateMachine.Condition = _Components._StateMachine.BasicMovementStates.Walk
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.2)
 		velocity.z = lerp(velocity.z, 0.0, 0.2)
 		is_walking = false
+		if !IsRunning:
+			_Components._StateMachine.Condition = _Components._StateMachine.BasicMovementStates.Idle
 
 
 	if WantJump:
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y = JumpVel
+			_Components._StateMachine.Jump()
+			velocity.x = lerp(velocity.x, Direction.x * 5, 1)
+			velocity.z = lerp(velocity.z, -Direction.z * 5, 1)
 		
 	if is_on_floor() and !WantJump:
 		WantJump = true
--- scenes/Entity/Player/PlayerComponents/StateMachineComponent.gd
+++ scenes/Entity/Player/PlayerComponents/StateMachineComponent.gd
@@ -4,7 +4,7 @@ extends Node
 
 @export_category("Nodes")
 @export var _Components : Components
-@export var _AnimationTree : AnimationTree
+@onready var Animator = %AnimationTree
 
 @export_category("Parameters")
 enum States {BasicMovement, CrouchMovement, HangMovement}
@@ -12,4 +12,20 @@ enum BasicMovementStates {Idle, Walk, Run}
 enum CrouchMovementStates {Idle, CrouchWalk, QuietWalk}
 enum HangMovement {Idle, GrabLeft, GrabRight}
 
-@export var State : States
+@export var State : States = States.BasicMovement
+@export var Condition = BasicMovementStates.Idle
+
+func _process(delta: float) -> void:
+	match State:
+		States.BasicMovement:
+			Animator.set("parameters/list/transition_request", "BasicMovement")
+			match Condition:
+				BasicMovementStates.Idle:
+					Animator.set("parameters/BasicMovement/transition_request", "Idle")
+				BasicMovementStates.Walk:
+					Animator.set("parameters/BasicMovement/transition_request", "Walk")
+				BasicMovementStates.Run:
+					Animator.set("parameters/BasicMovement/transition_request", "Run")
+					
+func Jump():
+	Animator.set("parameters/Jump/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
